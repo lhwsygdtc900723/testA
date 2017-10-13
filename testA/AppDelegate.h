5b82562da8a0b52d281dc91fe3b4f1b684f07e01
lihui <195814193@qq.com>
2017-10-13 11:33:24 +0800

    Initial Commit

---
+++ testA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testA
+//
+//  Created by lihui on 2017/10/13.
+//  Copyright © 2017年 lihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
